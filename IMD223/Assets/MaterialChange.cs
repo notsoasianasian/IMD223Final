93b9ecdece74f7a9f85a20b1aa85f25d51a2252b
notsoasianasian <44407935+notsoasianasian@users.noreply.github.com>
2018-11-19 12:45:10 -0500

    update

---
+++ IMD223/Assets/MaterialChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour {
+
+	public Color[] colorBank;
+	public bool mesh;
+
+	// Use this for initialization
+	void Start () {
+
+		int Num = Random.Range (0, colorBank.Length);
+
+		if (mesh) {
+			MeshRenderer m = GetComponent<MeshRenderer> ();
+			m.material.color = colorBank [Num];
+		}
+	}
+	void onColliderEnter(Collider other)
+		{
+		other.GetComponent<MeshRenderer> ();
+		}
+
+	}
+
+
